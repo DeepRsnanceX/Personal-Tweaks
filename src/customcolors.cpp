8b18a8858acc987c1a05433aeb04b68a4e2711b1
Saritahhh <tusmuertos018@gmail.com>
2025-07-09 03:29:51 -0600

    ICON INFO AND STUFF

--- src/customcolors.cpp
+++ src/customcolors.cpp
@@ -5,34 +5,6 @@ using namespace geode::prelude;
 
 auto theMod = Mod::get();
 
-/*
-MAIN PARTICLES COLOR:
-- m_playerGroundParticles
-- m_landParticles0
-- m_landParticles1
-- m_vehicleGroundParticles
-
-FLY PARTICLES COLOR:
-- m_trailingParticles
-- m_shipClickParticles
-
-UFO CLICK PARTICLES COLOR:
-- m_ufoClickParticles
-
-ROBOT JUMP PARTICLES COLOR:
-- m_robotBurstParticles
-
-SWING FIRE PARTICLES COLOR:
-- m_swingBurstParticles1
-- m_swingBurstParticles2
-
-particle members:
-- m_tStartColor
-- m_tEndColor
-- m_tStartColorVar
-- m_tEndColorVar
-*/
-
 // misc settings
 auto useCustomColors = theMod->getSettingValue<bool>("enable-customcolors");
 auto tintTrail = theMod->getSettingValue<bool>("tint-trail");
@@ -61,11 +33,6 @@ auto p1MainParticlesStart = theMod->getSettingValue<cocos2d::ccColor4B>("p1-main
 auto p1MainParticlesEnd = theMod->getSettingValue<cocos2d::ccColor4B>("p1-main-particles-end");
 auto p2MainParticlesStart = theMod->getSettingValue<cocos2d::ccColor4B>("p2-main-particles-start");
 auto p2MainParticlesEnd = theMod->getSettingValue<cocos2d::ccColor4B>("p2-main-particles-end");
-auto tintFlyParticles = theMod->getSettingValue<bool>("tint-fly-particles");
-auto flyParticlesStart = theMod->getSettingValue<cocos2d::ccColor4B>("fly-particles-start");
-auto flyParticlesEnd = theMod->getSettingValue<cocos2d::ccColor4B>("fly-particles-end");
-auto shipClickParticlesStart = theMod->getSettingValue<cocos2d::ccColor4B>("shipclick-particles-start");
-auto shipClickParticlesEnd = theMod->getSettingValue<cocos2d::ccColor4B>("shipclick-particles-end");
 auto tintUfoClickParticles = theMod->getSettingValue<bool>("tint-ufo-click-particles");
 auto p1UfoClickParticlesStart = theMod->getSettingValue<cocos2d::ccColor4B>("p1-ufo-click-particles-start");
 auto p1UfoClickParticlesEnd = theMod->getSettingValue<cocos2d::ccColor4B>("p1-ufo-click-particles-end");
@@ -153,21 +120,6 @@ $on_mod(Loaded) {
     listenForSettingChanges("p2-main-particles-end", [](cocos2d::ccColor4B value) {
         p2MainParticlesEnd = value;
     });
-    listenForSettingChanges("tint-fly-particles", [](bool value) {
-        tintFlyParticles = value;
-    });
-    listenForSettingChanges("fly-particles-start", [](cocos2d::ccColor4B value) {
-        flyParticlesStart = value;
-    });
-    listenForSettingChanges("fly-particles-end", [](cocos2d::ccColor4B value) {
-        flyParticlesEnd = value;
-    });
-    listenForSettingChanges("shipclick-particles-start", [](cocos2d::ccColor4B value) {
-        shipClickParticlesStart = value;
-    });
-    listenForSettingChanges("shipclick-particles-end", [](cocos2d::ccColor4B value) {
-        shipClickParticlesEnd = value;
-    });
     listenForSettingChanges("tint-ufo-click-particles", [](bool value) {
         tintUfoClickParticles = value;
     });
@@ -182,24 +134,6 @@ $on_mod(Loaded) {
     });
     listenForSettingChanges("p2-ufo-click-particles-end", [](cocos2d::ccColor4B value) {
         p2UfoClickParticlesEnd = value;
-    });
-    listenForSettingChanges("tint-robot-jump-particles", [](bool value) {
-        tintRobotJumpParticles = value;
-    });
-    listenForSettingChanges("robot-jump-particles-start", [](cocos2d::ccColor4B value) {
-        robotJumpParticlesStart = value;
-    });
-    listenForSettingChanges("robot-jump-particles-end", [](cocos2d::ccColor4B value) {
-        robotJumpParticlesEnd = value;
-    });
-    listenForSettingChanges("tint-swing-burst-particles", [](bool value) {
-        tintSwingBurstParticles = value;
-    });
-    listenForSettingChanges("swing-burst-particles-start", [](cocos2d::ccColor4B value) {
-        swingBurstParticlesStart = value;
-    });
-    listenForSettingChanges("swing-burst-particles-end", [](cocos2d::ccColor4B value) {
-        swingBurstParticlesEnd = value;
     });
 }
 
@@ -225,18 +159,10 @@ class $modify(ColorsPlayer, PlayerObject) {
         cocos2d::ccColor4F p1MainParticleEnd = cocos2d::ccColor4F(p1MainParticlesEnd.r / 255.0f, p1MainParticlesEnd.g / 255.0f, p1MainParticlesEnd.b / 255.0f, p1MainParticlesEnd.a / 255.0f);
         cocos2d::ccColor4F p2MainParticleStart = cocos2d::ccColor4F(p2MainParticlesStart.r / 255.0f, p2MainParticlesStart.g / 255.0f, p2MainParticlesStart.b / 255.0f, p2MainParticlesStart.a / 255.0f);
         cocos2d::ccColor4F p2MainParticleEnd = cocos2d::ccColor4F(p2MainParticlesEnd.r / 255.0f, p2MainParticlesEnd.g / 255.0f, p2MainParticlesEnd.b / 255.0f, p2MainParticlesEnd.a / 255.0f);
-        cocos2d::ccColor4F flyParticleStart = cocos2d::ccColor4F(flyParticlesStart.r / 255.0f, flyParticlesStart.g / 255.0f, flyParticlesStart.b / 255.0f, flyParticlesStart.a / 255.0f);
-        cocos2d::ccColor4F flyParticleEnd = cocos2d::ccColor4F(flyParticlesEnd.r / 255.0f, flyParticlesEnd.g / 255.0f, flyParticlesEnd.b / 255.0f, flyParticlesEnd.a / 255.0f);
-        cocos2d::ccColor4F shipClickParticleStart = cocos2d::ccColor4F(shipClickParticlesStart.r / 255.0f, shipClickParticlesStart.g / 255.0f, shipClickParticlesStart.b / 255.0f, shipClickParticlesStart.a / 255.0f);
-        cocos2d::ccColor4F shipClickParticleEnd = cocos2d::ccColor4F(shipClickParticlesEnd.r / 255.0f, shipClickParticlesEnd.g / 255.0f, shipClickParticlesEnd.b / 255.0f, shipClickParticlesEnd.a / 255.0f);
         cocos2d::ccColor4F p1UfoClickParticleStart = cocos2d::ccColor4F(p1UfoClickParticlesStart.r / 255.0f, p1UfoClickParticlesStart.g / 255.0f, p1UfoClickParticlesStart.b / 255.0f, p1UfoClickParticlesStart.a / 255.0f);
         cocos2d::ccColor4F p1UfoClickParticleEnd = cocos2d::ccColor4F(p1UfoClickParticlesEnd.r / 255.0f, p1UfoClickParticlesEnd.g / 255.0f, p1UfoClickParticlesEnd.b / 255.0f, p1UfoClickParticlesEnd.a / 255.0f);
         cocos2d::ccColor4F p2UfoClickParticleStart = cocos2d::ccColor4F(p2UfoClickParticlesStart.r / 255.0f, p2UfoClickParticlesStart.g / 255.0f, p2UfoClickParticlesStart.b / 255.0f, p2UfoClickParticlesStart.a / 255.0f);
         cocos2d::ccColor4F p2UfoClickParticleEnd = cocos2d::ccColor4F(p2UfoClickParticlesEnd.r / 255.0f, p2UfoClickParticlesEnd.g / 255.0f, p2UfoClickParticlesEnd.b / 255.0f, p2UfoClickParticlesEnd.a / 255.0f);
-        cocos2d::ccColor4F robotJumpParticleStart = cocos2d::ccColor4F(robotJumpParticlesStart.r / 255.0f, robotJumpParticlesStart.g / 255.0f, robotJumpParticlesStart.b / 255.0f, robotJumpParticlesStart.a / 255.0f);
-        cocos2d::ccColor4F robotJumpParticleEnd = cocos2d::ccColor4F(robotJumpParticlesEnd.r / 255.0f, robotJumpParticlesEnd.g / 255.0f, robotJumpParticlesEnd.b / 255.0f, robotJumpParticlesEnd.a / 255.0f);
-        cocos2d::ccColor4F swingBurstParticleStart = cocos2d::ccColor4F(swingBurstParticlesStart.r / 255.0f, swingBurstParticlesStart.g / 255.0f, swingBurstParticlesStart.b / 255.0f, swingBurstParticlesStart.a / 255.0f);
-        cocos2d::ccColor4F swingBurstParticleEnd = cocos2d::ccColor4F(swingBurstParticlesEnd.r / 255.0f, swingBurstParticlesEnd.g / 255.0f, swingBurstParticlesEnd.b / 255.0f, swingBurstParticlesEnd.a / 255.0f);
         // uh
         cocos2d::ccColor4F varianceOverrider = cocos2d::ccColor4F(variancesOverride.r / 255.0f, variancesOverride.g / 255.0f, variancesOverride.b / 255.0f, variancesOverride.a / 255.0f);
     };
@@ -263,18 +189,10 @@ class $modify(ColorsPlayer, PlayerObject) {
         fields->p1MainParticleEnd = cocos2d::ccColor4F(p1MainParticlesEnd.r / 255.0f, p1MainParticlesEnd.g / 255.0f, p1MainParticlesEnd.b / 255.0f, p1MainParticlesEnd.a / 255.0f);
         fields->p2MainParticleStart = cocos2d::ccColor4F(p2MainParticlesStart.r / 255.0f, p2MainParticlesStart.g / 255.0f, p2MainParticlesStart.b / 255.0f, p2MainParticlesStart.a / 255.0f);
         fields->p2MainParticleEnd = cocos2d::ccColor4F(p2MainParticlesEnd.r / 255.0f, p2MainParticlesEnd.g / 255.0f, p2MainParticlesEnd.b / 255.0f, p2MainParticlesEnd.a / 255.0f);
-        fields->flyParticleStart = cocos2d::ccColor4F(flyParticlesStart.r / 255.0f, flyParticlesStart.g / 255.0f, flyParticlesStart.b / 255.0f, flyParticlesStart.a / 255.0f);
-        fields->flyParticleEnd = cocos2d::ccColor4F(flyParticlesEnd.r / 255.0f, flyParticlesEnd.g / 255.0f, flyParticlesEnd.b / 255.0f, flyParticlesEnd.a / 255.0f);
-        fields->shipClickParticleStart = cocos2d::ccColor4F(shipClickParticlesStart.r / 255.0f, shipClickParticlesStart.g / 255.0f, shipClickParticlesStart.b / 255.0f, shipClickParticlesStart.a / 255.0f);
-        fields->shipClickParticleEnd = cocos2d::ccColor4F(shipClickParticlesEnd.r / 255.0f, shipClickParticlesEnd.g / 255.0f, shipClickParticlesEnd.b / 255.0f, shipClickParticlesEnd.a / 255.0f);
         fields->p1UfoClickParticleStart = cocos2d::ccColor4F(p1UfoClickParticlesStart.r / 255.0f, p1UfoClickParticlesStart.g / 255.0f, p1UfoClickParticlesStart.b / 255.0f, p1UfoClickParticlesStart.a / 255.0f);
         fields->p1UfoClickParticleEnd = cocos2d::ccColor4F(p1UfoClickParticlesEnd.r / 255.0f, p1UfoClickParticlesEnd.g / 255.0f, p1UfoClickParticlesEnd.b / 255.0f, p1UfoClickParticlesEnd.a / 255.0f);
         fields->p2UfoClickParticleStart = cocos2d::ccColor4F(p2UfoClickParticlesStart.r / 255.0f, p2UfoClickParticlesStart.g / 255.0f, p2UfoClickParticlesStart.b / 255.0f, p2UfoClickParticlesStart.a / 255.0f);
         fields->p2UfoClickParticleEnd = cocos2d::ccColor4F(p2UfoClickParticlesEnd.r / 255.0f, p2UfoClickParticlesEnd.g / 255.0f, p2UfoClickParticlesEnd.b / 255.0f, p2UfoClickParticlesEnd.a / 255.0f);
-        fields->robotJumpParticleStart = cocos2d::ccColor4F(robotJumpParticlesStart.r / 255.0f, robotJumpParticlesStart.g / 255.0f, robotJumpParticlesStart.b / 255.0f, robotJumpParticlesStart.a / 255.0f);
-        fields->robotJumpParticleEnd = cocos2d::ccColor4F(robotJumpParticlesEnd.r / 255.0f, robotJumpParticlesEnd.g / 255.0f, robotJumpParticlesEnd.b / 255.0f, robotJumpParticlesEnd.a / 255.0f);
-        fields->swingBurstParticleStart = cocos2d::ccColor4F(swingBurstParticlesStart.r / 255.0f, swingBurstParticlesStart.g / 255.0f, swingBurstParticlesStart.b / 255.0f, swingBurstParticlesStart.a / 255.0f);
-        fields->swingBurstParticleEnd = cocos2d::ccColor4F(swingBurstParticlesEnd.r / 255.0f, swingBurstParticlesEnd.g / 255.0f, swingBurstParticlesEnd.b / 255.0f, swingBurstParticlesEnd.a / 255.0f);
         // uh
         fields->varianceOverrider = cocos2d::ccColor4F(variancesOverride.r / 255.0f, variancesOverride.g / 255.0f, variancesOverride.b / 255.0f, variancesOverride.a / 255.0f);
     }
@@ -298,18 +216,8 @@ class $modify(ColorsPlayer, PlayerObject) {
             m_landParticles1->m_tEndColorVar = f->varianceOverrider;
             m_vehicleGroundParticles->m_tStartColorVar = f->varianceOverrider;
             m_vehicleGroundParticles->m_tEndColorVar = f->varianceOverrider;
-            m_trailingParticles->m_tStartColorVar = f->varianceOverrider;
-            m_trailingParticles->m_tEndColorVar = f->varianceOverrider;
-            m_shipClickParticles->m_tStartColorVar = f->varianceOverrider;
-            m_shipClickParticles->m_tEndColorVar = f->varianceOverrider;
             m_ufoClickParticles->m_tStartColorVar = f->varianceOverrider;
             m_ufoClickParticles->m_tEndColorVar = f->varianceOverrider;
-            m_robotBurstParticles->m_tStartColorVar = f->varianceOverrider;
-            m_robotBurstParticles->m_tEndColorVar = f->varianceOverrider;
-            m_swingBurstParticles1->m_tStartColorVar = f->varianceOverrider;
-            m_swingBurstParticles1->m_tEndColorVar = f->varianceOverrider;
-            m_swingBurstParticles2->m_tStartColorVar = f->varianceOverrider;
-            m_swingBurstParticles2->m_tEndColorVar = f->varianceOverrider;
         }
 
         // particles
@@ -333,35 +241,12 @@ class $modify(ColorsPlayer, PlayerObject) {
             m_vehicleGroundParticles->m_tEndColor = f->p1PrimaryParticleEnd;
         }
 
-        if (tintFlyParticles && !m_isSwing) {
-            m_trailingParticles->m_tStartColor = f->flyParticleStart;
-            m_trailingParticles->m_tEndColor = f->flyParticleEnd;
-            m_shipClickParticles->m_tStartColor = f->shipClickParticleStart;
-            m_shipClickParticles->m_tEndColor = f->shipClickParticleEnd;
-        }
-
         if (tintUfoClickParticles) {
             m_ufoClickParticles->m_tStartColor = f->p1UfoClickParticleStart;
             m_ufoClickParticles->m_tEndColor = f->p1UfoClickParticleEnd;
         } else {
             m_ufoClickParticles->m_tStartColor = f->p1PrimaryParticleStart;
             m_ufoClickParticles->m_tEndColor = f->p1PrimaryParticleEnd;
-        }
-
-        if (tintRobotJumpParticles) {
-            m_robotBurstParticles->m_tStartColor = f->robotJumpParticleStart;
-            m_robotBurstParticles->m_tEndColor = f->robotJumpParticleEnd;
-        }
-
-        if (tintSwingBurstParticles) {
-            m_swingBurstParticles1->m_tStartColor = f->swingBurstParticleStart;
-            m_swingBurstParticles1->m_tEndColor = f->swingBurstParticleEnd;
-            m_swingBurstParticles2->m_tStartColor = f->swingBurstParticleStart;
-            m_swingBurstParticles2->m_tEndColor = f->swingBurstParticleEnd;
-            if (m_isSwing) {
-                m_trailingParticles->m_tStartColor = f->swingBurstParticleStart;
-                m_trailingParticles->m_tEndColor = f->swingBurstParticleEnd;
-            }
         }
 
         // icon effects
@@ -377,15 +262,19 @@ class $modify(ColorsPlayer, PlayerObject) {
                 m_regularTrail->setColor(p1TrailColor);
             }
         }
-        if (tintDashFire) {
-            m_dashSpritesContainer->setColor(p1DashFireColor);
-            m_dashParticles->m_tStartColor = f->p1DashFireParticleStart;
-            m_dashParticles->m_tEndColor = f->p1DashFireParticleEnd;
-        } else {
-            m_dashSpritesContainer->setColor(p1PrimaryColor);
-            m_dashParticles->m_tStartColor = f->p1PrimaryParticleStart;
-            m_dashParticles->m_tEndColor = f->p1PrimaryParticleEnd;
+
+        if (m_dashFireSprite) {
+            if (tintDashFire) {
+                m_dashFireSprite->setColor(p1DashFireColor);
+                m_dashParticles->m_tStartColor = f->p1DashFireParticleStart;
+                m_dashParticles->m_tEndColor = f->p1DashFireParticleEnd;
+            } else {
+                m_dashFireSprite->setColor(p1PrimaryColor);
+                m_dashParticles->m_tStartColor = f->p1PrimaryParticleStart;
+                m_dashParticles->m_tEndColor = f->p1PrimaryParticleEnd;
+            }
         }
+        
         if (m_ghostTrail) {
             if (tintGhostTrail) {
                 m_ghostTrail->m_color = p1GhostTrailColor;
@@ -402,9 +291,86 @@ class $modify(ColorsPlayer, PlayerObject) {
             m_spiderSprite->m_secondColor = p1SecondaryColor;
             m_spiderSprite->updateColors();
         }
-        // particle effects (todo)
         
         // ----------- PLAYER 2 -----------------
+
+        // ...existing code...
+        // ----------- PLAYER 2 -----------------
+
+        if (m_isSecondPlayer) {
+
+            // particles
+            if (tintMainParticles) {
+                m_playerGroundParticles->m_tStartColor = f->p2MainParticleStart;
+                m_playerGroundParticles->m_tEndColor = f->p2MainParticleEnd;
+                m_landParticles0->m_tStartColor = f->p2MainParticleStart;
+                m_landParticles0->m_tEndColor = f->p2MainParticleEnd;
+                m_landParticles1->m_tStartColor = f->p2MainParticleStart;
+                m_landParticles1->m_tEndColor = f->p2MainParticleEnd;
+                m_vehicleGroundParticles->m_tStartColor = f->p2MainParticleStart;
+                m_vehicleGroundParticles->m_tEndColor = f->p2MainParticleEnd;
+            } else {
+                m_playerGroundParticles->m_tStartColor = f->p2PrimaryParticleStart;
+                m_playerGroundParticles->m_tEndColor = f->p2PrimaryParticleEnd;
+                m_landParticles0->m_tStartColor = f->p2PrimaryParticleStart;
+                m_landParticles0->m_tEndColor = f->p2PrimaryParticleEnd;
+                m_landParticles1->m_tStartColor = f->p2PrimaryParticleStart;
+                m_landParticles1->m_tEndColor = f->p2PrimaryParticleEnd;
+                m_vehicleGroundParticles->m_tStartColor = f->p2PrimaryParticleStart;
+                m_vehicleGroundParticles->m_tEndColor = f->p2PrimaryParticleEnd;
+            }
+
+            if (tintUfoClickParticles) {
+                m_ufoClickParticles->m_tStartColor = f->p2UfoClickParticleStart;
+                m_ufoClickParticles->m_tEndColor = f->p2UfoClickParticleEnd;
+            } else {
+                m_ufoClickParticles->m_tStartColor = f->p2PrimaryParticleStart;
+                m_ufoClickParticles->m_tEndColor = f->p2PrimaryParticleEnd;
+            }
+
+            // icon effects
+            m_iconSprite->setColor(p2PrimaryColor);
+            m_iconSpriteSecondary->setColor(p2SecondaryColor);
+            m_vehicleSprite->setColor(p2PrimaryColor);
+            m_vehicleSpriteSecondary->setColor(p2SecondaryColor);
+            if (m_waveTrail) {
+                m_waveTrail->setColor(p2WaveColor);
+            }
+            if (m_regularTrail) {
+                if (tintTrail) {
+                    m_regularTrail->setColor(p2TrailColor);
+                }
+            }
+
+            if (m_dashFireSprite) {
+                if (tintDashFire) {
+                    m_dashFireSprite->setColor(p2DashFireColor);
+                    m_dashParticles->m_tStartColor = f->p2DashFireParticleStart;
+                    m_dashParticles->m_tEndColor = f->p2DashFireParticleEnd;
+                } else {
+                    m_dashFireSprite->setColor(p2PrimaryColor);
+                    m_dashParticles->m_tStartColor = f->p2PrimaryParticleStart;
+                    m_dashParticles->m_tEndColor = f->p2PrimaryParticleEnd;
+                }
+            }
+
+            if (m_ghostTrail) {
+                if (tintGhostTrail) {
+                    m_ghostTrail->m_color = p2GhostTrailColor;
+                } else {
+                    m_ghostTrail->m_color = p2PrimaryColor;
+                }
+            }
+            if (m_isRobot) {
+                m_robotSprite->m_color = p2PrimaryColor;
+                m_robotSprite->m_secondColor = p2SecondaryColor;
+                m_robotSprite->updateColors();
+            } else if (m_isSpider) {
+                m_spiderSprite->m_color = p2PrimaryColor;
+                m_spiderSprite->m_secondColor = p2SecondaryColor;
+                m_spiderSprite->updateColors();
+            }
+        }
 
     }
 };
---
+++ src/iconinfogetter.cpp
@@ -0,0 +1,167 @@
+#include <Geode/modify/GJGarageLayer.hpp>
+#include <hiimjustin000.more_icons/include/MoreIcons.hpp>
+#include "Geode/utils/cocos.hpp"
+#include <Geode/ui/BasedButtonSprite.hpp>
+#include <Geode/binding/FLAlertLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(ImSoLazyGarageLayer, GJGarageLayer) {
+    struct Fields {
+        std::vector<std::string> m_iconList;
+    };
+
+    void addIconToList(CCObject* sender) {
+        auto addedSpr = CCSprite::create("addToList.png"_spr);
+        auto fields = m_fields.self();
+
+        IconInfo* selectedIcon = MoreIcons::getIcon(m_selectedIconType);
+        std::string iconName = selectedIcon->shortName;
+
+        fields->m_iconList.push_back(iconName); 
+
+        Notification::create("Icon " + iconName + " added!", addedSpr, 0.30f)->show();
+    }
+
+    void printListOfIcons(CCObject* sender) {
+        auto fields = m_fields.self();
+        auto errSpr = CCSprite::createWithSpriteFrameName("exMark_001.png");
+        auto worked = CCSprite::createWithSpriteFrameName("GJ_completesIcon_001.png");
+        std::string dir = "C:\\Users\\PC\\Desktop\\MIX";
+        std::string filePath = dir + "\\chosen_icons.txt";
+        bool fileSaved = false;
+
+        // Try to create directory if it doesn't exist (C++17)
+        try {
+            std::filesystem::create_directories(dir);
+        } catch (...) {
+            geode::log::debug("Failed to create directory: {}", dir);
+        }
+
+        // Try to write to file
+        std::ofstream out(filePath);
+        if (out.is_open()) {
+            for (const auto& icon : fields->m_iconList) {
+                out << icon << "\n";
+            }
+            out.close();
+            fileSaved = true;
+            geode::log::debug("Icon list saved to {}", filePath);
+            Notification::create("Icon list saved to chosen_icons.txt!", worked, 0.5f)->show();
+        }
+
+        // Fallback: print to debug log if file failed
+        if (!fileSaved) {
+            std::string allIcons;
+            for (const auto& icon : fields->m_iconList) {
+                allIcons += icon + "\n";
+            }
+            geode::log::debug("Icon list:\n{}", allIcons);
+            Notification::create("Failed to save file! List printed to debug.", errSpr, 0.5f)->show();
+        }
+    }
+
+    void removeLastIconFromList(CCObject* sender) {
+        auto fields = m_fields.self();
+        auto err = CCSprite::createWithSpriteFrameName("exMark_001.png");
+        auto removedSpr = CCSprite::createWithSpriteFrameName("GJ_completesIcon_001.png");
+
+        if (!fields->m_iconList.empty()) {
+            std::string removed = fields->m_iconList.back();
+            fields->m_iconList.pop_back();
+            geode::log::debug("Removed icon: {}", removed);
+            Notification::create("Removed last icon!", removedSpr, 0.3f)->show();
+        } else {
+            geode::log::debug("Icon list is already empty.");
+            Notification::create(" Icon list is empty!", err, 0.3f)->show();
+        }
+    }
+
+    void countMixIcons(CCObject* sender) {
+        size_t cubeCount    = MoreIcons::getIcons(IconType::Cube).size();
+        size_t shipCount    = MoreIcons::getIcons(IconType::Ship).size();
+        size_t ballCount    = MoreIcons::getIcons(IconType::Ball).size();
+        size_t ufoCount     = MoreIcons::getIcons(IconType::Ufo).size();
+        size_t waveCount    = MoreIcons::getIcons(IconType::Wave).size();
+        size_t robotCount   = MoreIcons::getIcons(IconType::Robot).size();
+        size_t spiderCount  = MoreIcons::getIcons(IconType::Spider).size();
+        size_t swingCount   = MoreIcons::getIcons(IconType::Swing).size();
+        size_t jetpackCount = MoreIcons::getIcons(IconType::Jetpack).size();
+
+        size_t total = cubeCount + shipCount + ballCount + ufoCount + waveCount + robotCount + spiderCount + swingCount + jetpackCount;
+
+        std::string infoData = fmt::format(
+            "<cg>Cube</c>: {}\n<cp>Ship</c>: {}\n<cr>Ball</c>: {}\n<co>Ufo</c>: {}\n<cf>Wave</c>: {}\nRobot: {}\n<ca>Spider</c>: {}\n<cy>Swing</c>: {}\n<cp>Jetpack</c>: {}\n\n<cd>Total</c>: {}",
+            cubeCount, shipCount, ballCount, ufoCount, waveCount,
+            robotCount, spiderCount, swingCount, jetpackCount, total
+        );
+
+        FLAlertLayer::create("Lumity Mix Info", infoData.c_str(), "ok")->show();
+    }
+
+    bool init() {
+        if (!GJGarageLayer::init()) return false;
+        
+        auto buttonsMenu = CCMenu::create();
+        buttonsMenu->setLayout(
+            RowLayout::create()
+                ->setGap(4.f)
+                ->setAxisAlignment(AxisAlignment::End)
+                ->setAxisReverse(true)
+                ->setCrossAxisOverflow(false)
+        );
+        buttonsMenu->setContentSize({80.f, 40.f});
+        buttonsMenu->setPosition({m_playerObject->getPositionX() + 90.f, m_playerObject->getPositionY()});
+        buttonsMenu->setID("pt-buttons-menu"_spr);
+        this->addChild(buttonsMenu);
+
+        auto addSpr = CircleButtonSprite::createWithSprite("addToList.png"_spr, 1.0f, CircleBaseColor::Green, CircleBaseSize::Small);
+        auto printSpr = CircleButtonSprite::createWithSprite("printList.png"_spr, 1.0f, CircleBaseColor::Pink, CircleBaseSize::Small);
+        auto removeLastSpr = CircleButtonSprite::createWithSprite("removeLast.png"_spr, 1.0f, CircleBaseColor::DarkAqua, CircleBaseSize::Small);
+
+        auto addButton = CCMenuItemSpriteExtra::create(
+            addSpr,
+            this,
+            menu_selector(ImSoLazyGarageLayer::addIconToList)
+        );
+        auto printButton = CCMenuItemSpriteExtra::create(
+            printSpr, 
+            this, 
+            menu_selector(ImSoLazyGarageLayer::printListOfIcons)
+        );
+        auto removeLastButton = CCMenuItemSpriteExtra::create(
+            removeLastSpr,
+            this,
+            menu_selector(ImSoLazyGarageLayer::removeLastIconFromList)
+        );
+
+        buttonsMenu->addChild(addButton);
+        buttonsMenu->addChild(printButton);
+        buttonsMenu->addChild(removeLastButton);
+
+        buttonsMenu->updateLayout();
+
+        // icon counter
+        auto infoMenu = CCMenu::create();
+        infoMenu->setContentSize({20.f, 20.f});
+        infoMenu->setID("mix-info"_spr);
+        
+        auto infoSpr = CCSprite::create("mix-info.png"_spr);
+        auto infoButton = CCMenuItemSpriteExtra::create(infoSpr, this, menu_selector(ImSoLazyGarageLayer::countMixIcons));
+
+        infoMenu->addChild(infoButton);
+        infoButton->setPosition({10.f, 10.f});
+
+        auto selBg = this->getChildByID("select-background");
+
+        auto addX = selBg->getContentSize().width / 2;
+        auto addY = selBg->getContentSize().height / 2;
+
+        infoMenu->setPosition({selBg->getPositionX() + addX, selBg->getPositionY() + addY});
+        infoMenu->setScale(0.7f);
+
+        this->addChild(infoMenu, 2);
+
+        return true;
+    }
+};
